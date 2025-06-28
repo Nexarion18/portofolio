d54ccd758b595c1a4963a9ec468ba001a6ed1ad0
Nexarion <fendipermadi058@gmail.com>
2025-06-28 16:50:59 +0700

    api webhook

--- netlify/functions/discord.js
+++ netlify/functions/discord.js
@@ -1,6 +1,6 @@
-import fetch from 'node-fetch';
+const fetch = require('node-fetch');
 
-export const handler = async (event, context) => {
+exports.handler = async function (event, context) {
   if (event.httpMethod !== 'POST') {
     return {
       statusCode: 405,
