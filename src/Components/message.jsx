562b2872608e275be296bad06cbb5399f5f8efae
Nexarion <fendipermadi058@gmail.com>
2025-06-28 16:06:32 +0700

    setup style.css

--- src/Components/message.jsx
+++ src/Components/message.jsx
@@ -8,7 +8,7 @@ import {
   DialogFooter,
   DialogHeader,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/Components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
