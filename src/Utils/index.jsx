86fd31a9ab7ff428a64b6b4b388dfecbf8c4ef5d
Nexarion <fendipermadi058@gmail.com>
2025-06-28 15:56:02 +0700

    setup style.css

--- src/Utils/index.jsx
+++ src/Utils/index.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
-import '../Styles/Style.css'
+import '../Styles/style.css'
 import Body from '../Components/Body.jsx'
 
 createRoot(document.getElementById('root')).render(
